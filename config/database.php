cdb77d1d5933f1e2bb276341683d12a40b2ac2f0
Silverius383 <silverius.stanic@si.ukdw.ac.id>
2022-12-13 13:06:27 +0700

    sertifikat4

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') && env('APP_ENV') !== 'testing' && env('APP_ENV') !== 'local' ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => resource_path('certificates/DigiCertGlobalRootCA.crt.pem'),
                 PDO::ATTR_EMULATE_PREPARES => true,
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
          ]) : [],
         ],
 
