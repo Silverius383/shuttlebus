f53f6e52223853b54bb4a21d69c8924a6fcb05ba
Silverius383 <silverius.stanic@si.ukdw.ac.id>
2022-12-13 12:49:01 +0700

    sertifikat2

--- config/database.php
+++ config/database.php
@@ -58,10 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql')&& env('APP_ENV') !== 'testing' && env('APP_ENV') !== 'local' ? array_filter([
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-         ]) : [],
+            ]) : [],
         ],
 
         'pgsql' => [
